41cd01b1ad74f8b11e55eaeaff63f4851bb48f0b
damien <ancelin.damien@gmail.com>
2025-07-10 16:13:37 +0200

    feat(tests): add vitest setup and tests for DeleteAccountButton component
    - Updated package.json to include vitest and testing dependencies.
    - Created DeleteAccountButton.test.tsx with tests for rendering and functionality.
    - Added global test setup in setup.ts for jest-dom.
    - Configured vitest in vite.config.ts and created a separate vitest.config.ts for test environment settings.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
+  test: {
+    environment: 'node',
+    globals: true, // pour utiliser les fonctions expect, describe, etc. sans les importer
+    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  },
   plugins: [react()],
   resolve: {
     alias: {
